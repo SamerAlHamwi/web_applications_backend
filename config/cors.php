7c45952ecbe1e46ce00893a740dd5005f022c380
rafahdakakni <rafah.dakakni@gmail.com>
2025-12-13 22:07:26 +0300

    .

--- config/cors.php
+++ config/cors.php
@@ -14,6 +14,7 @@ return [
         'http://127.0.0.1:8080',
         'http://localhost:54607', // Flutter web default port
         'http://localhost:64090',
+        'http://localhost:59763',
         'http://localhost:*', // Flutter web default port
         'https://web.autotap.site', // Production domain
         // Add your Flutter web development URL
