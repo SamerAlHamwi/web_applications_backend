a35789876f47ec2b0a34b3e9716a3012f24f056d
Samer <samer.0280alhamwi@gmail.com>
2025-12-14 09:13:27 +0300

    update

--- config/cors.php
+++ config/cors.php
@@ -12,11 +12,17 @@ return [
         'http://localhost:8080',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:8080',
+        'http://localhost:54607', // Flutter web default port
+        'http://localhost:64090',
+        'http://localhost:*', // Flutter web default port
+        'https://web.autotap.site', // Production domain
         // Add your Flutter web development URL
         // Add your production domain when deploying
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '^http://localhost(:\d+)?$', // Allow localhost with or without port for development
+    ],
 
     'allowed_headers' => ['*'],
 
